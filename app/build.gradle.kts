76fb1a171e9b6078607a194c9b9bdcb98c233a7b
execution720 <JustTomGo@gmail.com>
2025-02-04 17:10:19 +0900

    for merge crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    // logger
+    implementation ("com.orhanobut:logger:2.2.0")
+
 }
